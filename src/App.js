cb046f11e0bc8480dbd0868fb555422758c4a02b
Daniel Plata <danielplata79@gmail.com>
2024-05-18 00:38:29 -0500

    added navbar & welcome compoents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { auth } from "./firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import ChatBox from "./components/ChatBox";
 import Welcome from "./components/Welcome";
+
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function App() {
   const [user] = useAuthState(auth);
@@ -11,13 +12,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      {!user ? (
-        <Welcome />
-      ) : (
-        <>
-          <ChatBox />
-        </>
-      )}
+      {!user ? <Welcome /> : <ChatBox />}
     </div>
   );
 }
