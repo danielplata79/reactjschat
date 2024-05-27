1de5aab5b0101e741510f932c94e744efb8eb76f
Daniel Plata <danielplata79@gmail.com>
2024-05-27 13:53:44 -0500

    fixe per user auth photoURL not displaying correctly

--- src/components/Message.js
+++ src/components/Message.js
@@ -7,11 +7,7 @@ const Message = ({ message }) => {
 
   return (
     <div className={`chat-bubble ${message.uid === user.uid ? "right" : ""}`}>
-      <img
-        className="chat-bubble__left"
-        src="{ message.avatar }"
-        alt="user avatar"
-      />
+      <img className="chat-bubble__left" src={message.avatar} alt="img avatar" referrerPolicy="no-referrer" />
       <div className="chat-bubble__right">
         <p className="user-name"> {message.name} </p>
         <p className="user-message"> {message.text} </p>
