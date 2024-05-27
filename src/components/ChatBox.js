586750e6e1cae4e30c6b337bd64263ce87b180d6
Daniel Plata <danielplata79@gmail.com>
2024-05-26 22:43:44 -0500

    created chatbox useEffect that runs every time someone sends or delets a message

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,10 +1,32 @@
 import React, { useEffect, useState, useRef } from "react";
 import Message from "./Message";
 import SendMessage from "./SendMessage";
-
+import { db } from "../firebase";
 import { query, collection, orderBy, onSnapshot, limit } from "firebase/firestore";
 
 const ChatBox = () => {
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "messages"),
+      orderBy("createdAt", "desc"),
+      limit(50)
+    );
+
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      const fetchedMessages = [];
+      QuerySnapshot.forEach((doc) => {
+        fetchedMessages.push({ ...doc.data(), id: doc.id });
+      });
+
+      const sortedMessages = fetchedMessages.sort(
+        (a, b) => a.createdAt - b.createdAt
+      );
+      setMessages(sortedMessages);
+    });
+    return () => unsubscribe;
+  }, []);
+
   return (
     <main className="chat-box">
       <div className="messages-wrapper">
