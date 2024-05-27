d5472e86a4e2bf43856784d25b553278c702eabe
Daniel Plata <danielplata79@gmail.com>
2024-05-26 21:38:58 -0500

    created message state

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth, db } from "../firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 const SendMessage = () => {
+  const [message, setMessage] = useState("");
+
   return (
     <form className="send-message">
       <label htmlFor="messageInput" hidden> Enter Message </label>
-      <input id="messageInput" name="messageInput" type="text" className="form-input__input" placeholder="Type message.." />
+      <input id="messageInput" name="messageInput" type="text" className="form-input__input" placeholder="Type message.." value={message} onChange={(e) => setMessage(e.target.value)} />
       <button type="submit">Send</button>
     </form>
   );
