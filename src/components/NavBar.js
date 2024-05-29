17307c9d06c2cd4154b490fccb78749c0fd4fd3e
Daniel Plata <danielplata79@gmail.com>
2024-05-29 08:21:51 -0500

    fixed welcome.js, and starting customizing chatbox

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,13 @@ import React, { useState } from "react";
 import GoogleSignin from "../img/btn_google_signin_dark_pressed_web.png";
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { GoogleAuthProvider, signInWithRedirect, signInWithPopup } from "firebase/auth";
-
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const NavBar = () => {
   const [user] = useAuthState(auth);
 
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
-    //signInWithRedirect(auth, provider);
     signInWithPopup(auth, provider); // workaround for firefox enhanced coockie protection
   };
 
@@ -26,8 +24,9 @@ const NavBar = () => {
         <h1>OpenRChat//</h1>
       </span>
       {user ? (
-        <button onClick={signOut} className="sign-out" type="button">
-          Sign Out
+        <button onClick={signOut} className="sign-in-google" type="button">
+          <img src="/account-logout-512.png" width={30} />
+          <p>Sign Out</p>
         </button>
       ) : (
         <button className="sign-in-google" onClick={googleSignIn}>
