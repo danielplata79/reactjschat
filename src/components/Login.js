a9f52b5156aa0521f22330d2a5444167ef1bd550
Daniel Plata <danielplata79@gmail.com>
2024-06-01 15:25:04 -0500

    fixed login on mobile3

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "./Footer"
 import "./Login.css";
 import { auth } from "../firebase";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
@@ -27,34 +28,39 @@ const Login = () => {
   };
 
   return (
-    <main className="welcome">
-      <div className="header-div">
-        <h2>Welcome to OpenRChat, Developed by Daniel Plata</h2>
-        <img src="/realogo.png" alt="ReactJs logo" width={200} height={200} />
-        <h3>Open-Source, Cloud Based Realtime Chat Application</h3>
+    <main id="main">
+      <div className="header">
+        <div className="header_content">
+          <h1>Welcome to OpenRChat, Developed by Daniel Plata</h1>
+          <img src="/realogo.png" alt="ReactJs logo" width={200} height={200} />
+          <h2>Open-Source, Cloud Based Realtime Chat Application</h2>
+        </div>
       </div>
 
-      <div id="sign-in-container">
-        <div className="sign-in-wrapper">
-          <button className="sign-in-btn" onClick={googleSignIn}>
-            <img src="/google-plus-512.png" width={30} />
+      <div id="content">
+        <div className="sign-in">
+          <button className="sign-in--btn" onClick={googleSignIn}>
+            <img src="/google-plus-512.png" />
             <p>Sign in with Google</p>
           </button>
           <hr />
 
-          <button className="sign-in-btn" onClick={facebookSignIn}>
-            <img src="/facebook-3-512.png" width={30} />
+          <button className="sign-in--btn" onClick={facebookSignIn}>
+            <img src="/facebook-3-512.png" />
             <p>Sign in with Facebook</p>
           </button>
           <hr />
 
-          <a className="sign-in-btn" href="https://github.com/danielplata79">
-            <img src="/github-9-512.png" width={30} />
+          <a className="sign-in--btn" href="https://github.com/danielplata79">
+            <img src="/github-9-512.png" width={20} />
             <p>See my Github</p>
           </a>
         </div>
       </div>
+
+      <Footer />
     </main >
+
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./Login.css";
 import GoogleSignin from "../img/btn_google_signin_dark_pressed_web.png";
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -24,12 +25,12 @@ const Navbar = () => {
         <h1>OpenRChat//</h1>
       </span>
       {user ? (
-        <button onClick={signOut} className="sign-in-btn" type="button">
+        <button onClick={signOut} className="sign-in--btn" type="button">
           <img src="/account-logout-512.png" width={30} />
           <p>Sign Out</p>
         </button>
       ) : (
-        <button className="sign-in-btn" onClick={googleSignIn}>
+        <button className="sign-in--btn" onClick={googleSignIn}>
           <img src="/google-plus-512.png" width={30} />
           <p>Sign in with Google</p>
         </button>
