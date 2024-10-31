98a269eeea9a138f040875a31dba7d308f96edd1
Daniel Plata <danielplata79@gmail.com>
2024-10-30 23:07:20 -0500

    implemented firebase createUserWithEmailAndPassword sucesfully, also added password verification, email verification and username verification

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,9 +1,37 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 import "./Login.css";
 
 const CreateAccount = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [alert, setAlert] = useState("");
+
+  const createNewUser = (e) => {
+    e.preventDefault();
+    setAlert("");
+
+    if (password !== confirmPassword) {
+      setAlert("Passwords do not match");
+      return;
+    }
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+      const user = userCredential.user;
+      setAlert("User Created Succesfully!");
+    }).catch((error) => {
+      if (error.code === "auth/weak-password") {
+        setAlert("Password is Weak");
+      } if (error.code === "auth/email-already-in-use") {
+        setAlert("Email already in use");
+      }
+      console.log(error.code);
+    })
+  }
 
   return (
     <main id="main">
@@ -14,11 +42,15 @@ const CreateAccount = () => {
           <hr />
 
           <form>
-            <input name="user" type="text" placeholder="User Name" className="sign-in--input" />
-            <input name="password" type="password" placeholder="Password" className="sign-in--input" />
-            <input name="confirmpassword" type="text" placeholder="Confirm Password" className="sign-in--input" />
-            <button id="submit-btn-login" type="submit" className="sign-in--btn" >Create Account</button>
+            <input name="username" type="text" placeholder="User Name" className="sign-in--input" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input name="email" type="text" placeholder="Email" className="sign-in--input" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input name="password" type="password" placeholder="Password" className="sign-in--input" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input name="confirmpassword" type="password" placeholder="Confirm Password" className="sign-in--input" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+            <button id="submit-btn-login" type="submit" className="sign-in--btn" onClick={createNewUser} >Create Account</button>
           </form>
+          <br />
+          <br />
+          {alert && <p>{alert}</p>}
         </div>
       </div>
     </main>
