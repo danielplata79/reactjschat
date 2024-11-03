19f5b343529deb1d03baee20162dd162c7859c84
Daniel Plata <danielplata79@gmail.com>
2024-11-02 23:57:57 -0500

    creating contacts component 1

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,7 +1,27 @@
 import React from "react";
 import "./Contacts.css"
+import { fetchContacts, addContacts } from "../services/ContactService.js";
 
-const Contacts = () => {
+const Contacts = ({ userId }) => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const loadContacts = async () => {
+      const contactsData = await fectchContacts(userId)
+      setContacts(contactsData);
+    }
+  })
+
+  return (
+    <div>
+      <h1>Your Contacts</h1>
+      <ul>
+        {contacts.map(contact => (
+          <li key={contact.id}>{contact.username}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Contacts;
