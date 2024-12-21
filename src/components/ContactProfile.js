aa931fb62dfb1a0beeb7afba48fc39e79b00d40e
Daniel Plata <danielplata79@gmail.com>
2024-12-20 23:34:17 -0500

    feat(ContactProfile) Created ContactProfile component and stylesheet

---
+++ src/components/ContactProfile.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import { auth, db } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { useUserStore } from "../lib/userStore"
+import { useAuthState } from "react-firebase-hooks/auth";
+import "./Profile.css";
+import { ReactComponent as StatusIcon } from './status2.svg';
+
+import { ref, uploadBytes, getDownloadURL, deleteObject } from "firebase/storage";
+import { updateDoc } from "firebase/firestore";
+import { storage } from "../firebase";
+
+const Profile = () => {
+  const { currentUser } = useUserStore();
+
+  const updateAvatar = async (e) => {
+
+    try {
+      const imageFile = e.target.files[0];
+
+      const userDocRef = doc(db, "users", currentUser.id);
+      const userDoc = await getDoc(userDocRef);
+      const userData = userDoc.data();
+
+      const newAvatarPath = `avatars/${currentUser.id}/${imageFile.name}`;
+      const newImageRef = ref(storage, newAvatarPath);
+
+      if (userData.avatarPath) {
+        const oldProfilePicRef = ref(storage, userData.avatarPath);
+        deleteObject(oldProfilePicRef);
+      }
+
+      await uploadBytes(newImageRef, imageFile);
+
+      const newAvatarUrl = await getDownloadURL(newImageRef);
+
+      await updateDoc(userDocRef, {
+        avatarUrl: newAvatarUrl,
+        avatarPath: newAvatarPath
+      });
+
+      window.location.reload();
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <div className="profile-component--container">
+      <div className="profile-component--img">
+        <span>
+          <img src={currentUser.avatarUrl || currentUser.avatar || "/guest-500.png"} />
+          <span className="upload-image">
+            <input type="file"
+              onChange={(e) => {
+                updateAvatar(e)
+              }
+              } />
+            <img src="./img.png" alt="img" />
+          </span>
+        </span>
+
+      </div >
+      <div className="profile-component--info">
+        <h1>{currentUser.name}</h1>
+        <div className="profile-component--info-data">
+          <span>
+            <span><StatusIcon style={{ fill: '#80D999', height: '30px', width: '30px' }} /> </span>
+            <p><strong className="strong--txt">Status: </strong> {currentUser.status}</p>
+          </span>
+          <span>
+            <span><img src="./email.png" alt="email" /></span>
+            <p><strong className="strong--txt">Email: </strong> {currentUser.email}</p>
+          </span>
+          <span>
+            <span><img src="./numsign.png" alt="codetag" /></span>
+            <p><strong className="strong--txt">CodeTag: </strong> {currentUser.codetag} </p>
+          </span>
+          <span>
+            <span><img src="./date.png" alt="date" /></span>
+            <p><strong className="strong--txt">Member Since: </strong> {currentUser.createdAt.toDate().toDateString()} </p>
+          </span>
+        </div>
+      </div>
+    </div >
+  )
+}
+
+export default Profile;
