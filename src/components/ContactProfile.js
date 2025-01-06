581011f9ccf51738073fd9e4a0f85aaeb4a93c46
Daniel Plata <danielplata79@gmail.com>
2025-01-06 11:48:06 -0500

    Style(ContactProfile, ChatList) Fixed Heights problems on desktop and mobile when navbar is displayed

--- src/components/ContactProfile.js
+++ src/components/ContactProfile.js
@@ -2,37 +2,42 @@ import React from "react";
 import { useContactStore } from "../lib/contactStore";
 import "./Profile.css";
 import { ReactComponent as StatusIcon } from './status2.svg';
+import Navbar from "./Navbar";
 
 const Profile = () => {
   const { currentContact } = useContactStore();
 
   return (
     <div className="profile-component--container">
-      <div className="profile-component--img">
-        <span>
-          <img src={currentContact.avatarUrl || currentContact.avatar || "/guest-500.png"} />
-        </span>
+      <Navbar />
+      <div className="profile-component--containers">
 
-      </div >
-      <div className="profile-component--info">
-        <h1>{currentContact.name}</h1>
-        <div className="profile-component--info-data">
+        <div className="profile-component--img">
           <span>
-            <span><StatusIcon style={{ fill: '#80D999', height: '30px', width: '30px' }} /> </span>
-            <p><strong className="strong--txt">Status: </strong> {currentContact.status}</p>
+            <img src={currentContact.avatarUrl || currentContact.avatar || "/guest-500.png"} />
           </span>
-          <span>
-            <span><img src="./email.png" alt="email" /></span>
-            <p><strong className="strong--txt">Email: </strong> {currentContact.email}</p>
-          </span>
-          <span>
-            <span><img src="./numsign.png" alt="codetag" /></span>
-            <p><strong className="strong--txt">CodeTag: </strong> {currentContact.codetag} </p>
-          </span>
-          <span>
-            <span><img src="./date.png" alt="date" /></span>
-            <p><strong className="strong--txt">Member Since: </strong> {currentContact.createdAt.toDate().toDateString()} </p>
-          </span>
+
+        </div >
+        <div className="profile-component--info">
+          <h1>{currentContact.name}</h1>
+          <div className="profile-component--info-data">
+            <span>
+              <span><StatusIcon style={{ fill: '#80D999', height: '30px', width: '30px' }} /> </span>
+              <p><strong className="strong--txt">Status: </strong> {currentContact.status}</p>
+            </span>
+            <span>
+              <span><img src="./email.png" alt="email" /></span>
+              <p><strong className="strong--txt">Email: </strong> {currentContact.email}</p>
+            </span>
+            <span>
+              <span><img src="./numsign.png" alt="codetag" /></span>
+              <p><strong className="strong--txt">CodeTag: </strong> {currentContact.codetag} </p>
+            </span>
+            <span>
+              <span><img src="./date.png" alt="date" /></span>
+              <p><strong className="strong--txt">Member Since: </strong> {currentContact.createdAt.toDate().toDateString()} </p>
+            </span>
+          </div>
         </div>
       </div>
     </div >
